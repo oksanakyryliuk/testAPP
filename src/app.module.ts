09ca5c235db493c7abf27d6091e4a21d24b457cf
okyryliuk <o.kyryliuk.i@gmail.com>
2023-09-05 00:09:10 +0300

    config mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import {ConfigService, ConfigModule} from '@nestjs/config';
 import appConfig from './common/config/app.config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -9,7 +10,14 @@ import appConfig from './common/config/app.config';
       envFilePath: [".env",".development.env", " prod"],
       isGlobal: true
     }),
-
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get<string>('MONGODB_URI'),
+        };
+      },
+      inject: [ConfigService],
+    }),
 
   ],
   controllers: [],
